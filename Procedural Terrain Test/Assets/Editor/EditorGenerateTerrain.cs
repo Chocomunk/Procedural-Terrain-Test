8c4dfda95d19c064f07ec94c6ce24e828ad9069d
Alvin On <alvin.zhou.on@gmail.com>
2015-12-28 17:10:53 -0800

    First Commit
    First version of working procedural terrain using Unity's built in
    Mathf.PerlinNoise() function. Has three separate terrain calculations:
    p1 (ground), p2 (rocks), and p3 (hills). Other terrain aspects such as
    mountains or valleys can be added easily. The current map is made of 9
    terrains with 3 on each side. The dimension of the whole map is 500x500
    units, with a max height of 1000 units. To change these values make sure
    to apply ratio of change on the height to the scale of the noise, and to
    apply the ratio of change on the dimension to the frequency of noise.

---
+++ Procedural Terrain Test/Assets/Editor/EditorGenerateTerrain.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(UnityPerlinGenerator))]
+public class EditorGenerateTerrain : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		
+		UnityPerlinGenerator myScript = (UnityPerlinGenerator)target;
+		if(GUILayout.Button("Generate Terrain"))
+		{
+			myScript.Generate();
+		}
+		if(GUILayout.Button("Clear Children"))
+		{
+			while(myScript.transform.childCount>0){
+				foreach(Transform tr in myScript.transform){
+					MonoBehaviour.DestroyImmediate(tr.gameObject);
+				}
+			}
+		}
+	}
+}
---
+++ Procedural Terrain Test/Assets/UnityPerlinGenerator.cs
@@ -0,0 +1,187 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityPerlinGenerator : MonoBehaviour {
+
+	public const int MAP_SIZE = 513;
+	public static int dim = 3;
+
+	public float maxHeight = 2;
+	public float dimension = 200;
+	[Range(-1,1)]public float hardChange = 0;
+	public float tileOffsetX = 0;
+	public float tileOffsetZ = 0;
+
+	public float groundNoiseScale = 1;
+	public float groundScaleBuffer = 1;
+	public float groundNoiseFactor = 1;
+	public float groundFrequencyDuller = 1;
+	public float groundFrequencyReducer = 0;
+	public float groundReduceBuffer = 1;
+
+	public float rockNoiseScale = 1;
+	public float rockScaleBuffer = 1;
+	public float rockNoiseFactor = 1;
+	public float rockFrequencyDuller = 1;
+	public float rockFrequencyReducer = 0;
+	public float rockReduceBuffer = 1;
+	
+	public float hillNoiseScale = 1;
+	public float hillScaleBuffer = 1;
+	public float hillNoiseFactor = 1;
+	public float hillFrequencyDuller = 1;
+	public float hillFrequencyReducer = 0;
+	public float hillReduceBuffer = 1;
+
+	public Terrain[,] Terrains = new Terrain[dim,dim];
+	public Vector3 initPos;
+	public float[,] heightMapTable = new float[MAP_SIZE*dim,MAP_SIZE*dim];
+	
+	void Start(){
+		this.Generate();
+	}
+
+//	void OnDrawGizmos(){
+//		Gizmos.color = new Color(0,0,1,0.25f);
+//
+//		for(int i=0; i<MAP_SIZE*dim; i++){
+//			for(int j=0; j<MAP_SIZE*dim; j++){
+//				Gizmos.DrawCube(new Vector3(i*dimension/(MAP_SIZE*dim), (heightMapTable[i,j]-hardReduce)*maxHeight, j*dimension/(MAP_SIZE*dim)), 
+//				                new Vector3(dimension/(MAP_SIZE*dim),dimension/(MAP_SIZE*dim),dimension/(MAP_SIZE*dim)));
+//			}
+//		}
+//	}
+
+	public void Generate(){
+		CalcTerrain();
+		
+		for(int i=0; i<dim; i++){
+			for(int j=0; j<dim; j++){
+				TerrainData terrainData = new TerrainData();
+				terrainData.heightmapResolution = MAP_SIZE;
+				terrainData.size = new Vector3(dimension/dim, maxHeight, dimension/dim);
+				terrainData.SetHeights(0,0,generatePartitionedTable(i,j));
+
+				GameObject terrainObj = Terrain.CreateTerrainGameObject(terrainData);
+				terrainObj.transform.position = new Vector3(initPos.x+(i*((dimension/dim)-1)),initPos.y,initPos.z+(j*((dimension/dim)-1)));
+
+				terrainObj.transform.parent = this.transform;
+				Terrains[i,j] = terrainObj.GetComponent<Terrain>();
+			}
+		}
+
+		for(int i=0; i<dim; i++){
+			for(int j=0; j<dim; j++){
+				calcNeighbors(i,j,Terrains[i,j]);
+				Terrains[i,j].Flush();
+			}
+		}
+	}
+
+	public  void CalcTerrain(){
+		float i_offset=0, j_offset=0;
+		for(int i=0; i<MAP_SIZE*dim; i++){
+			for(int j=0; j<MAP_SIZE*dim; j++){
+				
+				if(i%MAP_SIZE==0)
+					i_offset+=0;
+				if(j%MAP_SIZE==0)
+					j_offset+=0;
+
+				float x = i+i_offset;
+				float z = j+j_offset;
+
+				float p1 = -1*groundFrequencyReducer/groundReduceBuffer + 
+					Mathf.PerlinNoise(
+						x*groundNoiseFactor/groundFrequencyDuller,z*groundNoiseFactor/groundFrequencyDuller
+						)*groundNoiseScale/groundScaleBuffer;
+
+				float p2 = -1*rockFrequencyReducer/rockReduceBuffer + 
+					Mathf.PerlinNoise(
+						x*rockNoiseFactor/rockFrequencyDuller,z*rockNoiseFactor/rockFrequencyDuller
+						)*rockNoiseScale/rockScaleBuffer;
+
+				float p3 = -1*hillFrequencyReducer/hillReduceBuffer + 
+					Mathf.PerlinNoise(
+						x*hillNoiseFactor/hillFrequencyDuller,z*hillNoiseFactor/hillFrequencyDuller
+						)*hillNoiseScale/hillScaleBuffer;
+
+				if(p1<0)p1=0;
+				if(p2<0)p2=0;
+				if(p3<0)p3=0;
+
+				heightMapTable[j,i] = p1+p2+p3 -hardChange;
+//				heightMapTable[i,j] = 0.1f*maxHeight;
+//				Debug.Log(i+", "+j+": " + heightMapTable[i,j]);
+			}
+		}
+	}
+
+	float[,] generatePartitionedTable(int x_index, int y_index){
+		float[,] table = new float[MAP_SIZE,MAP_SIZE];
+//		for(int i=0; i<MAP_SIZE; i++){
+//			for(int j=0; j<MAP_SIZE; j++){
+//				table[i,j] = heightMapTable[(x_index*MAP_SIZE)+i,(y_index*MAP_SIZE)+j];
+//			}
+//		}
+		for(int i=0; i<MAP_SIZE; i++){
+			for(int j=0; j<MAP_SIZE; j++){
+				table[i,j] = heightMapTable[((x_index)*MAP_SIZE)+j,((y_index)*MAP_SIZE)+i];
+			}
+		}
+		return table;
+	}
+
+	void calcNeighbors(int x_index, int y_index, Terrain terrain){
+		Terrain left = null;
+		Terrain top = null;
+		Terrain right = null;
+		Terrain bottom = null;
+
+		if(x_index == 0){
+			right = Terrains[1,y_index];
+		}else if(x_index == dim-1){
+			left = Terrains[dim-1,y_index];
+		}else{
+			right = Terrains[x_index+1,y_index];
+			left = Terrains[x_index-1,y_index];
+		}
+		
+		if(y_index == 0){
+			top = Terrains[x_index,1];
+		}else if(y_index == dim-1){
+			bottom = Terrains[x_index,dim-1];
+		}else{
+			top = Terrains[x_index,y_index+1];
+			bottom = Terrains[x_index,y_index-1];
+		}
+
+		terrain.SetNeighbors(left,top,right,bottom);
+		fixNeighbors(terrain,right,top);
+	}
+
+	void fixNeighbors(Terrain currTerrain, Terrain right, Terrain top){
+		float[,] newHeights = new float[MAP_SIZE, MAP_SIZE];
+		float[,] rightHeights = new float[MAP_SIZE, MAP_SIZE], topHeights = new float[MAP_SIZE, MAP_SIZE];
+		
+		if (right != null)
+			rightHeights = right.terrainData.GetHeights(0, 0, MAP_SIZE, MAP_SIZE);
+		if (top != null)
+			topHeights = top.terrainData.GetHeights(0, 0, MAP_SIZE, MAP_SIZE);
+		
+		if (right != null || top != null)
+		{
+			newHeights = currTerrain.terrainData.GetHeights(0, 0, MAP_SIZE, MAP_SIZE);
+			
+			for (int i = 0; i < MAP_SIZE; i++)
+			{
+				if (right != null)
+					newHeights[i, MAP_SIZE - 1] = rightHeights[i, 0];
+				
+				if (top != null)
+					newHeights[MAP_SIZE - 1, i] = topHeights[0, i];
+			}
+			currTerrain.terrainData.SetHeights(0, 0, newHeights);
+		}
+	}
+}
